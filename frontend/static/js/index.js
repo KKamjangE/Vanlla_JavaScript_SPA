7a7ae463ecfa7745ded521f1b92dca4b771ad811
KKamjangE <ajm980718@gmail.com>
2023-04-18 21:51:27 +0900

    feat: history 구현

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,9 @@
+// 페이지 전환 함수
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
 // async를 사용하는 이유는, 어떤 페이지에서는 렌더링 전에 서버 단 요청을 먼저 받아야하는 경우가 있기 때문
 const router = async () => {
   // 각 route의 경로와 현재 페이지 확인용 콘솔
@@ -28,6 +34,16 @@ const router = async () => {
   console.log(match.route.view());
 };
 
+// 뒤로가기나 새로고침했을 때 router도 그 페이지에 맞게 동작
+window.addEventListener("popstate", router);
+
 document.addEventListener("DOMContentLoaded", () => {
+  // 클릭 이벤트 발생 시 해당 target이 "data-link" attribute가 있다면 페이지 이동 함수 실행
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
   router();
 });
