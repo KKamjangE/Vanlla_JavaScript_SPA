a02e67df50f46eea5cace0ea9ecc037bbd89f5af
KKamjangE <ajm980718@gmail.com>
2023-04-18 21:53:31 +0900

    feat: 페이지 초기 세팅 구현

---
+++ frontend/static/views/AbstractView.js
@@ -0,0 +1,14 @@
+// 페이지 초기 세팅
+export default class {
+  constructor() {}
+
+  // 페이지 타이틀
+  setTitle(title) {
+    document.title = title;
+  }
+
+  // 뿌려질 HTML
+  async getHtml() {
+    return "";
+  }
+}
